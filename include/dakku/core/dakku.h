952b246aa0df06adc8ad213f52c338af3a0e08f3
xehoth <824547322@qq.com>
2021-10-01 20:03:11 +0800

    add dakku

---
+++ include/dakku/core/dakku.h
@@ -0,0 +1,43 @@
+//
+// Created by xehoth on 2021/10/1.
+//
+
+#ifndef DAKKU_INCLUDE_DAKKU_H_
+#define DAKKU_INCLUDE_DAKKU_H_
+#include <type_traits>
+#include <limits>
+#include <concepts>
+
+namespace dakku {
+
+#ifdef DAKKU_FLOAT_AS_DOUBLE
+using Float = double;
+#else
+using Float = float;
+#endif
+
+constexpr Float INF = std::numeric_limits<Float>::infinity();
+constexpr Float SHADOW_EPS = static_cast<Float>(1e-4);
+
+class Scene;
+class Integrator;
+class SamplerIntegrator;
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Vector2;
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Vector3;
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Point2;
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Point3;
+template <typename T>
+requires std::is_arithmetic_v<T>
+class Normal3;
+class Ray;
+}  // namespace dakku
+
+#endif  // DAKKU_INCLUDE_DAKKU_H_
