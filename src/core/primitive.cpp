73b60fc8ff22d7321698dbf95fcb6cbc0697d0db
xehoth <824547322@qq.com>
2022-03-23 13:20:27 +0800

    add primitive

--- src/core/primitive.cpp
+++ src/core/primitive.cpp
@@ -1,6 +1,7 @@
 #include <core/primitive.h>
 #include <core/logger.h>
-#include <core/state.h>
+#include <core/material.h>
+#include <core/interaction.h>
 
 DAKKU_BEGIN
 
@@ -13,18 +14,46 @@ bool GeometricPrimitive::intersectP(const Ray &) const {
   return false;
 }
 
-void GeometricPrimitive::serialize(Json &json, OutputStream *stream) const {
-  // TODO: optimize this
-  std::string shapeName;
-  for (const auto &[name, p] : renderState.shapes) {
-    if (p.get() == shape) {
-      shapeName = name;
-    }
-  }
-  json["shape"] = shapeName;
-
+void GeometricPrimitive::computeScatteringFunctions(SurfaceInteraction &si,
+                                                    MemoryArena &arena) const {
+  if (material) material->computeScatteringFunctions(si, arena);
+  DAKKU_CHECK(
+      si.n.dot(si.shading.n) > 0,
+      "geometric normal and shading normal are not in the same direction");
 }
 
-void GeometricPrimitive::unserialize(const Json &json, InputStream *stream) {}
+const AreaLight *GeometricPrimitive::getAreaLight() const { return areaLight; }
 
+const Material *GeometricPrimitive::getMaterial() const { return material; }
+
+void GeometricPrimitive::construct(const Shape *_shape,
+                                   const Material *_material,
+                                   const AreaLight *_areaLight) {
+  this->shape = _shape;
+  this->material = _material;
+  this->areaLight = _areaLight;
+}
+
+void GeometricPrimitive::serialize(Json &, OutputStream *) const {
+  DAKKU_ERR(
+      "dakku now only support triangle mesh primitive, to be implemented!");
+}
+
+void GeometricPrimitive::unserialize(const Json &, InputStream *) {
+  DAKKU_ERR(
+      "dakku now only support triangle mesh primitive, to be implemented!");
+}
+
+const AreaLight *Aggregate::getAreaLight() const {
+  DAKKU_ERR("use GeometricPrimitive::getAreaLight() instead!");
+  return nullptr;
+}
+const Material *Aggregate::getMaterial() const {
+  DAKKU_ERR("use GeometricPrimitive::getMaterial() instead!");
+  return nullptr;
+}
+void Aggregate::computeScatteringFunctions(SurfaceInteraction &,
+                                           MemoryArena &) const {
+  DAKKU_ERR("use GeometricPrimitive::computeScatteringFunctions instead!");
+}
 DAKKU_END
--- src/core/primitive.h
+++ src/core/primitive.h
@@ -5,22 +5,51 @@
 DAKKU_BEGIN
 class Primitive : public SerializableObject {
  public:
+  DAKKU_DECLARE_OBJECT(Primitive);
+
   virtual bool intersect(const Ray &r, SurfaceInteraction &) const = 0;
   [[nodiscard]] virtual bool intersectP(const Ray &r) const = 0;
+  virtual void computeScatteringFunctions(SurfaceInteraction &si,
+                                          MemoryArena &arena) const = 0;
+  [[nodiscard]] virtual const AreaLight *getAreaLight() const = 0;
+  [[nodiscard]] virtual const Material *getMaterial() const = 0;
 };
 
 class GeometricPrimitive : public Primitive {
  public:
   DAKKU_DECLARE_OBJECT(GeometricPrimitive);
 
+  void construct(const Shape *shape, const Material *material,
+                 const AreaLight *areaLight);
+
   bool intersect(const Ray &r, SurfaceInteraction &) const override;
   [[nodiscard]] bool intersectP(const Ray &r) const override;
+
+  void computeScatteringFunctions(SurfaceInteraction &si,
+                                  MemoryArena &arena) const override;
+  [[nodiscard]] const AreaLight *getAreaLight() const override;
+  [[nodiscard]] const Material *getMaterial() const override;
+  [[nodiscard]] const Shape *getShape() const { return shape; }
+
   void serialize(Json &json, OutputStream *stream) const override;
   void unserialize(const Json &json, InputStream *stream) override;
 
+  friend class RenderState;
+
  private:
-  Shape *shape{};
-  AreaLight *areaLight{};
+  const Shape *shape{};
+  const Material *material{};
+  const AreaLight *areaLight{};
+};
+
+class Aggregate : public Primitive {
+ public:
+  DAKKU_DECLARE_OBJECT(Aggregate);
+
+  [[nodiscard]] const AreaLight *getAreaLight() const override;
+  [[nodiscard]] const Material *getMaterial() const override;
+  void computeScatteringFunctions(SurfaceInteraction &si,
+                                  MemoryArena &arena) const override;
 };
 DAKKU_END
 #endif  // DAKKU_SRC_CORE_PRIMITIVE_H_
