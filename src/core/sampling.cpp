c536d6df824f82113a0a8f9d3db139d7b4aec07f
xehoth <824547322@qq.com>
2022-03-23 22:57:45 +0800

    add sampling

--- src/core/sampling.cpp
+++ src/core/sampling.cpp
@@ -5,4 +5,23 @@ Point2f uniformSampleTriangle(const Point2f &u) {
   Float su0 = std::sqrt(u[0]);
   return Point2f(1 - su0, u[1] * su0);
 }
+
+Point2f concentricSampleDisk(const Point2f &u) {
+  // map uniform random numbers to [-1, 1] ^ 2
+  Point2f uOffset = 2 * u - Vector2f(1, 1);
+
+  // handle degeneracy at the origin
+  if (uOffset.x() == 0 && uOffset.y() == 0) return Point2f(0, 0);
+
+  // apply concentric mapping to point
+  Float theta, r;
+  if (std::abs(uOffset.x()) > std::abs(uOffset.y())) {
+    r = uOffset.x();
+    theta = PI_OVER_4 * (uOffset.y() / uOffset.x());
+  } else {
+    r = uOffset.y();
+    theta = PI_OVER_2 - PI_OVER_4 * (uOffset.x() / uOffset.y());
+  }
+  return r * Point2f(std::cos(theta), std::sin(theta));
+}
 DAKKU_END
--- src/core/sampling.h
+++ src/core/sampling.h
@@ -10,5 +10,23 @@ DAKKU_BEGIN
  */
 Point2f uniformSampleTriangle(const Point2f &u);
 
+/**
+ * sample disk
+ * @param u 2d random number
+ */
+Point2f concentricSampleDisk(const Point2f &u);
+
+/**
+ * cosine weighted sample hemisphere
+ * @param u 2d random number
+ * @return the sampled w in the hemisphere
+ */
+inline Vector3f cosineSampleHemisphere(const Point2f &u) {
+  Point2f d = concentricSampleDisk(u);
+  Float z = std::sqrt(
+      std::max(static_cast<Float>(0), 1 - d.x() * d.x() - d.y() * d.y()));
+  return Vector3f(d.x(), d.y(), z);
+}
+
 DAKKU_END
 #endif  // DAKKU_SRC_CORE_SAMPLING_H_
