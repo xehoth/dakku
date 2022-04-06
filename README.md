90c3579d2e081ddce7236cf92fe953082c28e4fe
xehoth <824547322@qq.com>
2022-04-06 21:28:16 +0800

    update vector

--- README.md
+++ README.md
@@ -3,12 +3,10 @@
 [![build](https://github.com/xehoth/dakku/actions/workflows/build.yml/badge.svg)](https://github.com/xehoth/dakku/actions/workflows/build.yml) [![doc](https://github.com/xehoth/dakku/actions/workflows/doc.yml/badge.svg)](https://github.com/xehoth/dakku/actions/workflows/doc.yml)
 
 **Warning: This project is currently under developing and does not guarantee any consistency.**  
-The main branch is being refactored, you can see [the old version here](https://github.com/xehoth/dakku/tree/old).
+**The main branch is being refactored, you can see [the old version here](https://github.com/xehoth/dakku/tree/old).**
 
 ## About
 
 **Dakku** is a **physically based** renderer for the purpose of learning global illumination and ray tracing.   
 Dakku is written in **modern C++** (>= 20) and integrates [embree](https://github.com/embree/embree) for high performance computing.   
 The project structure is similar to [pbrt-v3](https://github.com/mmp/pbrt-v3) but the implementation is very different.
-
-![](docs/images/cornell_box.png)
--- src/math/vector.h
+++ src/math/vector.h
@@ -4,11 +4,21 @@
 
 namespace dakku {
 
+/**
+ * @brief vector
+ * 
+ * @tparam T data type
+ * @tparam S size
+ */
 template <ArithmeticType T, size_t S>
 class Vector : public VectorBase<T, S, Vector<T, S>> {
  public:
   using VectorBase<T, S, Vector<T, S>>::VectorBase;
 
+  /**
+   * @brief subtraction between vectors
+   *
+   */
   friend Vector operator-(const Vector &a, const Vector &b) {
     Vector ret = a;
     ret -= b;
@@ -16,44 +26,114 @@ class Vector : public VectorBase<T, S, Vector<T, S>> {
   }
 };
 
+/**
+ * @brief point
+ * 
+ * @tparam T data type
+ * @tparam S size
+ */
 template <ArithmeticType T, size_t S>
 class Point : public VectorBase<T, S, Point<T, S>> {
  public:
   using VectorBase<T, S, Point<T, S>>::VectorBase;
 
+  /**
+   * @brief subtraction between two points yields vectors
+   *
+   */
   friend Vector<T, S> operator-(const Point &a, const Point &b) {
     return Vector<T, S>(a) - Vector<T, S>(b);
   }
+
+  /**
+   * @brief point - vector => point
+   *
+   */
+  friend Point operator-(const Point &a, const Vector<T, S> &b) {
+    return a - Point(b);
+  }
+
+  /**
+   * @brief point + vector => point
+   *
+   */
+  friend Point operator+(const Point &a, const Vector<T, S> &b) {
+    return a + Point(b);
+  }
 };
 
+/**
+ * @brief normal
+ * 
+ * @tparam T data type
+ * @tparam S size
+ */
 template <ArithmeticType T, size_t S>
 class Normal : public VectorBase<T, S, Normal<T, S>> {
  public:
   using VectorBase<T, S, Normal<T, S>>::VectorBase;
-  
-   friend Normal operator-(const Normal &a, const Normal &b) {
+
+  /**
+   * @brief subtraction between two normals
+   *
+   */
+  friend Normal operator-(const Normal &a, const Normal &b) {
     Normal ret = a;
     ret -= b;
     return ret;
   }
 };
 
+/**
+ * @brief 2d vectors
+ * 
+ * @tparam T data type
+ */
 template <ArithmeticType T>
 using Vector2 = Vector<T, 2>;
+
+/**
+ * @brief 3d vectors
+ * 
+ * @tparam T data type
+ */
 template <ArithmeticType T>
 using Vector3 = Vector<T, 3>;
+
+/// 2d float vector
 using Vector2f = Vector2<float>;
+/// 3d float vector
 using Vector3f = Vector3<float>;
 
+/**
+ * @brief 2d point
+ * 
+ * @tparam T data type
+ */
 template <ArithmeticType T>
 using Point2 = Point<T, 2>;
+
+/**
+ * @brief 3d point
+ * 
+ * @tparam T data type
+ */
 template <ArithmeticType T>
 using Point3 = Point<T, 3>;
+
+/// 2d float point
 using Point2f = Point2<float>;
+/// 3d float point
 using Point3f = Point3<float>;
 
+/**
+ * @brief 3d normal
+ * 
+ * @tparam T data type
+ */
 template <ArithmeticType T>
 using Normal3 = Normal<T, 3>;
+/// 3d float normal
 using Normal3f = Normal3<float>;
 }  // namespace dakku
 #endif
